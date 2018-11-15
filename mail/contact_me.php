e5ba532b82ec7de0072cad9e810e28f4e2c949b7
Marc Wyss <wyss.a.marc@gmail.com>
2018-11-15 11:18:51 +0100

    testing php with logs

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,7 +1,7 @@
 <?php
 // Check for empty fields
 
-error_log("lkjsdflkjsdflkj");
+
 
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
@@ -12,6 +12,9 @@ if(empty($_POST['name'])  		||
 	echo "No arguments Provided!";
 	return false;
    }
+
+   error_log("---------------- LOG 1 -----------------");
+
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $phone = $_POST['phone'];
@@ -22,6 +25,12 @@ $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "sg@polygravity.io\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
-mail($to,$email_subject,$email_body,$headers);
+error_log("---------------- LOG 2----------------- + $to ");
+
+//mail($to,$email_subject,$email_body,$headers);
+mail($to, 'hello hello', 'body');
+
+error_log("---------------- LOG 3 -----------------");
+
 return true;
 ?>
